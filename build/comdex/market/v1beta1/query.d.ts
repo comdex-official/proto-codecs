fece8596e51381dc35fc87af5e1ba6d3c57cbc99
srinu391 <srinu@comdex.sg>
2022-11-10 12:50:13 +0530

    Generate new js proto for asset and markets.

--- build/comdex/market/v1beta1/query.d.ts
+++ build/comdex/market/v1beta1/query.d.ts
@@ -178,6 +178,7 @@ export declare const QueryMarketsResponse: {
             currentIndex?: string | number | Long.Long | undefined;
             isPriceActive?: boolean | undefined;
             priceValue?: (string | number | Long.Long)[] | undefined;
+            discardedHeightDiff?: string | number | Long.Long | undefined;
         }[] | undefined;
         pagination?: {
             nextKey?: Uint8Array | undefined;
@@ -191,6 +192,7 @@ export declare const QueryMarketsResponse: {
             currentIndex?: string | number | Long.Long | undefined;
             isPriceActive?: boolean | undefined;
             priceValue?: (string | number | Long.Long)[] | undefined;
+            discardedHeightDiff?: string | number | Long.Long | undefined;
         }[] & ({
             assetId?: string | number | Long.Long | undefined;
             scriptId?: string | number | Long.Long | undefined;
@@ -198,6 +200,7 @@ export declare const QueryMarketsResponse: {
             currentIndex?: string | number | Long.Long | undefined;
             isPriceActive?: boolean | undefined;
             priceValue?: (string | number | Long.Long)[] | undefined;
+            discardedHeightDiff?: string | number | Long.Long | undefined;
         } & {
             assetId?: string | number | (Long.Long & {
                 high: number;
@@ -490,6 +493,64 @@ export declare const QueryMarketsResponse: {
                 toUnsigned: () => Long.Long;
                 xor: (other: string | number | Long.Long) => Long.Long;
             } & Record<Exclude<keyof I["timeWeightedAverage"][number]["priceValue"][number], keyof Long.Long>, never>))[] & Record<Exclude<keyof I["timeWeightedAverage"][number]["priceValue"], keyof (string | number | Long.Long)[]>, never>) | undefined;
+            discardedHeightDiff?: string | number | (Long.Long & {
+                high: number;
+                low: number;
+                unsigned: boolean;
+                add: (addend: string | number | Long.Long) => Long.Long;
+                and: (other: string | number | Long.Long) => Long.Long;
+                compare: (other: string | number | Long.Long) => number;
+                comp: (other: string | number | Long.Long) => number;
+                divide: (divisor: string | number | Long.Long) => Long.Long;
+                div: (divisor: string | number | Long.Long) => Long.Long;
+                equals: (other: string | number | Long.Long) => boolean;
+                eq: (other: string | number | Long.Long) => boolean;
+                getHighBits: () => number;
+                getHighBitsUnsigned: () => number;
+                getLowBits: () => number;
+                getLowBitsUnsigned: () => number;
+                getNumBitsAbs: () => number;
+                greaterThan: (other: string | number | Long.Long) => boolean;
+                gt: (other: string | number | Long.Long) => boolean;
+                greaterThanOrEqual: (other: string | number | Long.Long) => boolean;
+                gte: (other: string | number | Long.Long) => boolean;
+                isEven: () => boolean;
+                isNegative: () => boolean;
+                isOdd: () => boolean;
+                isPositive: () => boolean;
+                isZero: () => boolean;
+                lessThan: (other: string | number | Long.Long) => boolean;
+                lt: (other: string | number | Long.Long) => boolean;
+                lessThanOrEqual: (other: string | number | Long.Long) => boolean;
+                lte: (other: string | number | Long.Long) => boolean;
+                modulo: (other: string | number | Long.Long) => Long.Long;
+                mod: (other: string | number | Long.Long) => Long.Long;
+                multiply: (multiplier: string | number | Long.Long) => Long.Long;
+                mul: (multiplier: string | number | Long.Long) => Long.Long;
+                negate: () => Long.Long;
+                neg: () => Long.Long;
+                not: () => Long.Long;
+                notEquals: (other: string | number | Long.Long) => boolean;
+                neq: (other: string | number | Long.Long) => boolean;
+                or: (other: string | number | Long.Long) => Long.Long;
+                shiftLeft: (numBits: number | Long.Long) => Long.Long;
+                shl: (numBits: number | Long.Long) => Long.Long;
+                shiftRight: (numBits: number | Long.Long) => Long.Long;
+                shr: (numBits: number | Long.Long) => Long.Long;
+                shiftRightUnsigned: (numBits: number | Long.Long) => Long.Long;
+                shru: (numBits: number | Long.Long) => Long.Long;
+                subtract: (subtrahend: string | number | Long.Long) => Long.Long;
+                sub: (subtrahend: string | number | Long.Long) => Long.Long;
+                toInt: () => number;
+                toNumber: () => number;
+                toBytes: (le?: boolean | undefined) => number[];
+                toBytesLE: () => number[];
+                toBytesBE: () => number[];
+                toSigned: () => Long.Long;
+                toString: (radix?: number | undefined) => string;
+                toUnsigned: () => Long.Long;
+                xor: (other: string | number | Long.Long) => Long.Long;
+            } & Record<Exclude<keyof I["timeWeightedAverage"][number]["discardedHeightDiff"], keyof Long.Long>, never>) | undefined;
         } & Record<Exclude<keyof I["timeWeightedAverage"][number], keyof TimeWeightedAverage>, never>)[] & Record<Exclude<keyof I["timeWeightedAverage"], keyof {
             assetId?: string | number | Long.Long | undefined;
             scriptId?: string | number | Long.Long | undefined;
@@ -497,6 +558,7 @@ export declare const QueryMarketsResponse: {
             currentIndex?: string | number | Long.Long | undefined;
             isPriceActive?: boolean | undefined;
             priceValue?: (string | number | Long.Long)[] | undefined;
+            discardedHeightDiff?: string | number | Long.Long | undefined;
         }[]>, never>) | undefined;
         pagination?: ({
             nextKey?: Uint8Array | undefined;
@@ -645,6 +707,7 @@ export declare const QueryMarketResponse: {
             currentIndex?: string | number | Long.Long | undefined;
             isPriceActive?: boolean | undefined;
             priceValue?: (string | number | Long.Long)[] | undefined;
+            discardedHeightDiff?: string | number | Long.Long | undefined;
         } | undefined;
     } & {
         timeWeightedAverage?: ({
@@ -654,6 +717,7 @@ export declare const QueryMarketResponse: {
             currentIndex?: string | number | Long.Long | undefined;
             isPriceActive?: boolean | undefined;
             priceValue?: (string | number | Long.Long)[] | undefined;
+            discardedHeightDiff?: string | number | Long.Long | undefined;
         } & {
             assetId?: string | number | (Long.Long & {
                 high: number;
@@ -946,6 +1010,64 @@ export declare const QueryMarketResponse: {
                 toUnsigned: () => Long.Long;
                 xor: (other: string | number | Long.Long) => Long.Long;
             } & Record<Exclude<keyof I["timeWeightedAverage"]["priceValue"][number], keyof Long.Long>, never>))[] & Record<Exclude<keyof I["timeWeightedAverage"]["priceValue"], keyof (string | number | Long.Long)[]>, never>) | undefined;
+            discardedHeightDiff?: string | number | (Long.Long & {
+                high: number;
+                low: number;
+                unsigned: boolean;
+                add: (addend: string | number | Long.Long) => Long.Long;
+                and: (other: string | number | Long.Long) => Long.Long;
+                compare: (other: string | number | Long.Long) => number;
+                comp: (other: string | number | Long.Long) => number;
+                divide: (divisor: string | number | Long.Long) => Long.Long;
+                div: (divisor: string | number | Long.Long) => Long.Long;
+                equals: (other: string | number | Long.Long) => boolean;
+                eq: (other: string | number | Long.Long) => boolean;
+                getHighBits: () => number;
+                getHighBitsUnsigned: () => number;
+                getLowBits: () => number;
+                getLowBitsUnsigned: () => number;
+                getNumBitsAbs: () => number;
+                greaterThan: (other: string | number | Long.Long) => boolean;
+                gt: (other: string | number | Long.Long) => boolean;
+                greaterThanOrEqual: (other: string | number | Long.Long) => boolean;
+                gte: (other: string | number | Long.Long) => boolean;
+                isEven: () => boolean;
+                isNegative: () => boolean;
+                isOdd: () => boolean;
+                isPositive: () => boolean;
+                isZero: () => boolean;
+                lessThan: (other: string | number | Long.Long) => boolean;
+                lt: (other: string | number | Long.Long) => boolean;
+                lessThanOrEqual: (other: string | number | Long.Long) => boolean;
+                lte: (other: string | number | Long.Long) => boolean;
+                modulo: (other: string | number | Long.Long) => Long.Long;
+                mod: (other: string | number | Long.Long) => Long.Long;
+                multiply: (multiplier: string | number | Long.Long) => Long.Long;
+                mul: (multiplier: string | number | Long.Long) => Long.Long;
+                negate: () => Long.Long;
+                neg: () => Long.Long;
+                not: () => Long.Long;
+                notEquals: (other: string | number | Long.Long) => boolean;
+                neq: (other: string | number | Long.Long) => boolean;
+                or: (other: string | number | Long.Long) => Long.Long;
+                shiftLeft: (numBits: number | Long.Long) => Long.Long;
+                shl: (numBits: number | Long.Long) => Long.Long;
+                shiftRight: (numBits: number | Long.Long) => Long.Long;
+                shr: (numBits: number | Long.Long) => Long.Long;
+                shiftRightUnsigned: (numBits: number | Long.Long) => Long.Long;
+                shru: (numBits: number | Long.Long) => Long.Long;
+                subtract: (subtrahend: string | number | Long.Long) => Long.Long;
+                sub: (subtrahend: string | number | Long.Long) => Long.Long;
+                toInt: () => number;
+                toNumber: () => number;
+                toBytes: (le?: boolean | undefined) => number[];
+                toBytesLE: () => number[];
+                toBytesBE: () => number[];
+                toSigned: () => Long.Long;
+                toString: (radix?: number | undefined) => string;
+                toUnsigned: () => Long.Long;
+                xor: (other: string | number | Long.Long) => Long.Long;
+            } & Record<Exclude<keyof I["timeWeightedAverage"]["discardedHeightDiff"], keyof Long.Long>, never>) | undefined;
         } & Record<Exclude<keyof I["timeWeightedAverage"], keyof TimeWeightedAverage>, never>) | undefined;
     } & Record<Exclude<keyof I, "timeWeightedAverage">, never>>(object: I): QueryMarketResponse;
 };
